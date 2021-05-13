995c2786f0725b7de91e7afa47db027db7a61035
system <rohinigaud2008@gmail.com>
2021-05-13 22:57:58 +0530

    This is my Project on Bus Ticket Management

--- AssignmentReal/src/com/realcoderz/shapes/AreaCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.realcoderz.shapes;
-
-public class AreaCalculator {
-	public static void main(String args[])
-	{
-	Rectangle area=new Rectangle();
-	area.calculateArea();
-	}
-}
--- AssignmentReal/src/com/realcoderz/shapes/Rectangle.java
+++
@@ -1,8 +0,0 @@
-package com.realcoderz.shapes;
-
-public class Rectangle {
-	void calculateArea()
-	{	
-	System.out.println("The Area of the rectangle is calculated using the formula length * breadth");
-	}
-}
